237e918f0c841a3c7a9b06c987e159c2cb0d3fb6
elaheh <elaheh@elahehs-macbook.local>
2017-08-17 10:24:05 -0400

    Initial Commit

---
+++ Vacations App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vacations App
+//
+//  Created by elaheh on 2017-08-17.
+//  Copyright © 2017 elaheh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
