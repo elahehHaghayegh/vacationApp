1b5030121aa5ac2b828a3cac2d1fb8af7957d47d
elaheh <elaheh@elahehs-macbook.local>
2017-08-17 11:00:25 -0400

    first map view appeared and show me the Airport of Montreal According to latitude and longitude

--- Vacations App/ViewController.swift
+++ Vacations App/ViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        GMSServices.provideAPIKey("AIzaSyDEDZ8ZriyZl1jc5TLUxHvbotfGvPcJxFE")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 45.469642, longitude: -73.744781, zoom: 12)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero , camera: camera)
+        
+        view = mapView
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
